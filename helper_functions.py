2ebb3f39297706a577f158bd3f03b7b429c5149b
CAPELLE Thomas <thomas.capelle@inria.fr>
2018-12-28 18:14:45 +0100

    added articles

--- helper_functions.py
+++ helper_functions.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from PIL import Image
 
+
 def show_image(img:Image, ax:plt.Axes=None, figsize:tuple=(3,3), hide_axis:bool=True, cmap:str='binary',alpha:float=None)->plt.Axes:
     if ax is None: fig,ax = plt.subplots(figsize=figsize)
     ax.imshow(img, cmap=cmap, alpha=alpha)
